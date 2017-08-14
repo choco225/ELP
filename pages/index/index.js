e506efffe0f98505a953c20f06218eb6fc972176
choco leung <sendtochoco@gmail.com>
2017-08-14 18:20:04 +0800

    elp home

---
+++ pages/index/index.js
@@ -0,0 +1,45 @@
+//index.js
+//获取应用实例
+var app = getApp()
+Page({
+  data: {
+    imgUrls: ['/photos/home-slide-image1.png', '/photos/home-slide-image2.png', '/photos/home-slide-image3.png']
+  },
+  changeIndicatorDots: function (e) {
+    this.setData({
+      indicatorDots: !this.data.indicatorDots
+    })
+  },
+  changeAutoplay: function (e) {
+    this.setData({
+      autoplay: !this.data.autoplay
+    })
+  },
+  intervalChange: function (e) {
+    this.setData({
+      interval: e.detail.value
+    })
+  },
+  durationChange: function (e) {
+    this.setData({
+      duration: e.detail.value
+    })
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad: function () {
+    console.log('onLoad')
+    var that = this
+    //调用应用实例的方法获取全局数据
+    app.getUserInfo(function(userInfo){
+      //更新数据
+      that.setData({
+        userInfo:userInfo
+      })
+    })
+  }
+})
