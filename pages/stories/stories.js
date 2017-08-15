6f4cd390c32513d9c6b1d5181c0def31868e6126
choco leung <sendtochoco@gmail.com>
2017-08-15 16:42:52 +0800

    elp update 15/8

---
+++ pages/stories/stories.js
@@ -0,0 +1,37 @@
+Page({
+  data: {
+    winWidth: 0,
+    winHeight: 0,
+    currentTab: 0,
+  },
+  onLoad: function () {
+    var that = this;
+    wx.getSystemInfo({
+      success: function (res) {
+        that.setData({
+          winWidth: res.windowWidth,
+          winHeight: res.windowHeight
+        });
+      }
+    });
+  },
+  bindChange: function (e) {
+    var that = this;
+    that.setData({ currentTab: e.detail.current });
+  },
+  swichNav: function (e) {
+    var that = this;
+    if (this.data.currentTab === e.target.dataset.current) {
+      return false;
+    } else {
+      that.setData({
+        currentTab: e.target.dataset.current
+      })
+    }
+  },
+  tz: function (e) {
+    wx.navigateTo({
+      url: '../story/story',
+    })
+    }
+})
